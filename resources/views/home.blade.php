e6d98a75df8b5d4e127726ba11dcdb1f8276fa6f
emmageo3 <kanfanyemma22@gmail.com>
2022-04-30 14:27:00 +0000

    mise en place de breeze

---
+++ resources/views/home.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('logo.png') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <title>LaFrancoise221</title>
+</head>
+<body>
+
+    <header>
+        <div class="logo">
+            La françoise 
+        </div>
+
+        <div class="navbar">
+
+            <div class="hamburger" id="hamburger">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </div>
+
+            <ul id="item">
+                <li>
+                    <a href="">Accueil</a>
+                </li>
+                <li>
+                    <a href="">Nouveautés</a>
+                </li>
+                <li>
+                    <a href="">Hommes</a>
+                </li>
+                <li>
+                    <a href="">Femmes</a>
+                </li>
+                <li>
+                    <a href="">Enfants</a>
+                </li>
+                <li>
+                    <a href="">Accessoires</a>
+                </li>
+                <li>
+                    <a href="">A propos</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <div class="barre">
+        <div class="recherche">
+            <a href="">Barre de recherche</a>
+        </div>
+        <div class="panier">
+            <a href="">Panier</a>
+        </div>
+    </div>
+
+    <div class="banniere">
+        
+    </div>
+
+    <section class="categories">
+        
+    </section>
+
+    
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+</html>
