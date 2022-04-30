012d092ad94fa9d45a0ab1d6ae0f32fd736b2dad
emmageo3 <kanfanyemma22@gmail.com>
2022-04-30 18:14:40 +0000

    relations des articles

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Categorie;
+use App\Models\Souscategorie;
+use App\Models\Collection;
 
 class Article extends Model
 {
     use HasFactory;
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categorie::class);
+    }
+
+    public function sous_categorie()
+    {
+        return $this->belongsTo(Souscategorie::class);
+    }
+
+    public function collection()
+    {
+        return $this->belongsTo(Collection::class);
+    }
 }
--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Article;
 
 class Categorie extends Model
 {
     use HasFactory;
+
+    public function articles() 
+{ 
+    return $this->hasMany(Article::class); 
+}
 }
